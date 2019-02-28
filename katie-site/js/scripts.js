22fd719950ce938d9c5e68c4e2264321eb3eb70b
mattatttaq <mattatttaq@gmail.com>
2019-02-27 22:28:21 -0500

    Character updated
    script.js updated

--- katie-site/js/scripts.js
+++ katie-site/js/scripts.js
@@ -14,12 +14,12 @@ jQuery( document ).ready(function() {
             if(a == 'ileum') {
                 $(img).attr('alt', a + '\'s profile');
                 img.attr('src','./img/char/ksilver_ileum.jpg');
-                text.text(a + 'Ileum: New to the reaper world Ileum is ready to reap his first soul. Ileum does not remember who he was before he was a reaper or that he had a life.  Ileum uses the different shoes to change how he looks to be more relatable to the souls he is guiding.');
+                text.text(a + ': New to the reaper world Ileum is ready to reap his first soul. Ileum does not remember who he was before he was a reaper or that he had a life.  Ileum uses the different shoes to change how he looks to be more relatable to the souls he is guiding.');
             } else if (a == 'joon') {
                 $(img).attr('alt', a + '\'s profile');
                 img.attr('src', './img/char/ksilver_joon.jpg');
                 console.log('joon');
-                text.text(a + ' Joon:  Joon was a back up dancer for the group Shine 5.  He idolized one of the singers named Jii, who gave him his shoes  to help him calm his nerves when he was auditioning. When Joon finds out that Jii took his own life Joon spirals out of control and is hit by a car. After being hit he spots Jii as a reaper, only to have Jii run away. He is now helping Ileum, who seems very familiar to Joon.');
+                text.text(a + ': Joon was a back up dancer for the group Shine 5.  He idolized one of the singers named Jii, who gave him his shoes  to help him calm his nerves when he was auditioning. When Joon finds out that Jii took his own life Joon spirals out of control and is hit by a car. After being hit he spots Jii as a reaper, only to have Jii run away. He is now helping Ileum, who seems very familiar to Joon.');
             } else if (a == 'deva') {
                 $(img).attr('alt', a + '\'s profile');
                 console.log('deva');
