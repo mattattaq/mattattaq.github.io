49dfebf22d5595a2855e86e5df092d16e4c3d2cc
mattatttaq <mattatttaq@gmail.com>
2019-04-28 15:45:09 -0400

    katie-site
    * relative pathing

--- katie-site/js/scripts.js
+++ katie-site/js/scripts.js
@@ -43,7 +43,7 @@ jQuery( document ).ready(function() {
     }
     if(jQuery("#comic-carousel")) {
         console.log('comic\'s page');
-        var requestUrl = "../js/pages-manifest.json";
+        var requestUrl = "./js/pages-manifest.json";
         var request = new XMLHttpRequest();
         request.open('GET', requestUrl);
         request.responseType = 'json';
