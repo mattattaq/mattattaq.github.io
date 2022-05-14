b7e68d26062eb2e602a3ddf6c0f41b7bc39c6d84
Mathew Allbright <>
2022-05-14 11:10:44 -0400

    [feature] - add Profile section and coin todo: Style updates and make the coin functional, good for display

--- mattattaq/src/components/header/Header.jsx
+++ mattattaq/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './header.scss'
 import {
   faEnvelope
 } from "@fortawesome/free-solid-svg-icons";
