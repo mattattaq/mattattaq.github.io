f260ceaba7e15c09c8798699110cde337758c80a
MattAttaq <mattatttaq@gmail.com>
2024-06-21 15:03:18 -0400

    refactor
    * refactor myjs, though these might need to be broken into functions on App.js.
    * Updates to Profile.jsx

--- mattattaq/src/components/profile/Profile.jsx
+++ mattattaq/src/components/profile/Profile.jsx
@@ -16,13 +16,14 @@ export default function Profile() {
                 <Row>
                     <Col lg={8}>
                         <p className="break lite">
-                            Welcome to my portfolio website hosted via <i>gh-pages</i>. Here is where I've got a <a href="#sec4"><i>timeline</i></a> of my accomplishments during and after college. It includes my first <a href="#gn"><i>graphic novel</i></a>, various conventions i've been to, and my job history that pertains to being a <i>web developer</i>. I also have examples of projects I've done for myself or work. You'll also find a link to my <a href="#gh"><i>github</i></a> account where I have some code examples.
+                        Welcome to my portfolio website, proudly hosted on <i>GitHub Pages</i>. Here, you'll find a detailed <a href="#sec4"><i>timeline</i></a> of my accomplishments during and after college. This includes my debut <a href="https://www.amazon.com/Reggie-Rabbit-Matthew-Albright/dp/0991528271/"><i>graphic novel</i></a>, my participation in various conventions, and a comprehensive overview of my career as a <i>web developer</i>.
+                        Explore examples of both personal and professional projects, showcasing my skills and creativity. Additionally, you can visit my <a href="https://github.com/mattattaq/" target='_blank' rel='noreferrer'><i>GitHub account</i></a> for more code samples and open-source contributions. If you have any questions or comments, please don't hesitate to contact me through the provided form or via my social media links above.
                         </p>
                         <p className="break lite">If you have any questions or comments you can contact me <i><a href="mailto:mattatttaq@gmail.com">here</a></i> or my social media links above.</p>
                     </Col>
                     <Col lg={4}>
                         <h5><b>Technology</b></h5>
-                        <p className="lite">Scala, MySql, React, Redux, JavaScript, jQuery, Sass, HTML/CSS, Bootstrap, Foundation, Tailwind, PHP, git</p>
+                        <p className="lite">Scala, MySql, React, Redux, JavaScript, jQuery, Sass/Scss, HTML/CSS, Bootstrap, Foundation, Tailwind, PHP, git</p>
                         <h5><b>Familiar With:</b></h5>
                         <p className="lite"> Next, Gulp, Brew, Webpack, Express, Node, Wordpress, Drupal, SalesForce Marketing Cloud</p>
                     </Col>
--- mattattaq/src/myjs.js
+++ mattattaq/src/myjs.js
@@ -1,59 +1,69 @@
 import $ from 'jquery';
 window.$ = $;
 
-$(document).ready(function(){
+$(document).ready(function () {
   console.log("js Fired!");
 
-  //Panel Animation
-  $('.panel').hover(function(){
-    console.log("hover Fired");
-    $(this).css("margin-left", "10px");
-    }, function(){
-    $(this).css("margin-left", "0px");
-  });
-  
-  //Coin Animation
-  var spinArray = ['animation900','animation1080','animation1260','animation1440','animation1620','animation1800','animation1980','animation2160'];
+  // Panel Animation
+  $('.panel').hover(
+    function () {
+      console.log("hover Fired");
+      $(this).css("margin-left", "10px");
+    },
+    function () {
+      $(this).css("margin-left", "0px");
+    }
+  );
+
+  // Coin Animation
+  const spinArray = [
+    'animation900', 'animation1080', 'animation1260',
+    'animation1440', 'animation1620', 'animation1800',
+    'animation1980', 'animation2160'
+  ];
 
   function getSpin() {
-  var spin = spinArray[Math.floor(Math.random()*spinArray.length)];
-  return spin;
+    return spinArray[Math.floor(Math.random() * spinArray.length)];
   }
 
-  $('#coin').on('click', function(){
+  $('#coin').on('click', function () {
+    $(this).removeClass();
 
-  $('#coin').removeClass();
-
-  setTimeout(function(){
-  $('#coin').addClass(getSpin());
-  }, 100);
-
+    setTimeout(() => {
+      $(this).addClass(getSpin());
+    }, 100);
   });
 
-//Smooth Scrolling
-$("a[href^='#']").on("click", function(e) {
-  e.preventDefault();
-  $("html, body").animate({
-    scrollTop: $($(this).attr("href")).offset().top
-  }, 1000);
+  // Smooth Scrolling
+  $("a[href^='#']").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top
+    }, 1000);
+  });
+
+  if ($(window.location.hash).length > 1) {
+    $("html, body").animate({
+      scrollTop: $(window.location.hash).offset().top
+    }, 1000);
+  }
+
+  // Toggle sticky "to-top" button
+  const $toTop = $('#to-top');
+  const elementPosition = $toTop.offset();
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > elementPosition.top) {
+      $toTop.css({
+        position: 'fixed',
+        bottom: '0',
+        visibility: 'visible'
+      });
+    } else {
+      $toTop.css({
+        position: 'absolute',
+        visibility: 'hidden'
+      });
+    }
+  });
 });
-
-if ($(window.location.hash).length > 1) {
-  $("html, body").animate({
-    scrollTop: $(window.location.hash).offset().top
-  }, 1000);
-}
-
-//toggle sticky
-var elementPosition = $('#to-top').offset();
-
-$(window).scroll(function(){
-        if($(window).scrollTop() > elementPosition.top){
-              $('#to-top').css('position','fixed').css('bottom','0').css('visibility','visible');
-        } else {
-            $('#to-top').css('position','absolute').css('visibility','hidden');
-        }
-});
-
-});
-
